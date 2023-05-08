3426df8b2d60a7c0163ad7158507d60080ffabe3
Magne Strømsnes <alaka2@gmail.com>
2023-05-08 15:12:49 +0200

    Get a reading and report what it is. Return to sender
    Reading is validated to conform to pydantic specification.

--- server.py
+++ server.py
@@ -1,12 +1,25 @@
 import logging
-
+import uvicorn
 from fastapi import FastAPI
+from sensor import SensorReading
+
+app = FastAPI()
+
+
+@app.post("/")
+def add_reading(reading: SensorReading):
+    logging.log(logging.INFO, reading)
+    return reading
+
+
 def main():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
 if __name__ == "__main__":
     main()
