0eb96cc15ff0640a9b8a3dee8b4d33ab0317b3d5
Magne Strømsnes <alaka2@gmail.com>
2023-05-08 12:39:01 +0200

    Add poetry configuration

---
+++ server.py
@@ -0,0 +1,12 @@
+import logging
+
+from fastapi import FastAPI
+def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+if __name__ == "__main__":
+    main()
