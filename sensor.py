85ed401965035ba206284743c55a01d10de445e3
Magne Strømsnes <alaka2@gmail.com>
2023-05-19 22:05:40 +0200

    Rework SensorData to use categorical
    The SensorType, Sensor and Unit columns of the dataframe should all be
    considered Categorical data, which is natively supported by pandas and
    can offer improved dataframe performance if used over the object dtype.

--- sensor.py
+++ sensor.py
@@ -1,10 +1,8 @@
 from enum import Enum
 from pydantic import BaseModel, validator
-import pandera.extensions
-import pandera.strategies
-import pandera
+import pandera as pa
+from pandera.typing import Index, Series
 import pandas as pd
-from datetime import datetime
 
 
 class MemberStrEnum(Enum):
@@ -72,6 +70,58 @@ class SensorReading(BaseModel):
         return {key: value for key, value in self if key not in self._indexes}
 
 
-@pandera.extensions.register_check_method(statistics=["enum"])
-def is_in_enum(df, *, enum: MemberStrEnum):
-    return df.isin(enum.values())
+def make_dtype_kwargs(enum):
+    return {"categories": enum.values(), "ordered": False}
+
+
+class SensorData(pa.DataFrameModel):
+    sensor_type: Index[pd.CategoricalDtype] = pa.Field(
+        dtype_kwargs=make_dtype_kwargs(SensorType), isin=SensorType.values()
+    )
+    sensor: Index[pd.CategoricalDtype] = pa.Field(
+        dtype_kwargs=make_dtype_kwargs(Sensor), isin=Sensor.values()
+    )
+    timestamp: Index[pa.DateTime]
+
+    reading: Series[float]
+    unit: Series[pd.CategoricalDtype] = pa.Field(
+        dtype_kwargs=make_dtype_kwargs(Unit), isin=Unit.values()
+    )
+
+    @classmethod
+    def repair_dataframe(cls, df) -> pd.DataFrame:
+        """The dataframe multiindex doesn't save correctly in parquet. The categorical types are dropped. To fix this, the index is first reset before saving, making them regular columns.
+        The columns do preserve the categorical types."""
+        df = cls._convert_columns(df)
+        df = df.set_index(SensorReading._indexes, drop=True)
+        return df
+
+    @staticmethod
+    def _convert_columns(df: pd.DataFrame) -> pd.DataFrame:
+        df["timestamp"] = pd.to_datetime(df["timestamp"])
+        df["sensor_type"] = pd.Categorical(
+            df["sensor_type"], **make_dtype_kwargs(SensorType)
+        )
+        df["sensor"] = pd.Categorical(df["sensor"], **make_dtype_kwargs(Sensor))
+        df["unit"] = pd.Categorical(df["unit"], **make_dtype_kwargs(Unit))
+        return df
+
+    @classmethod
+    def make_dataframe_from_list_of_readings(
+        cls,
+        readings: list[SensorReading],
+    ) -> pd.DataFrame:
+        if readings:
+            df = pd.DataFrame(map(lambda r: r.dict(), readings))
+            df = cls.repair_dataframe(df)
+        else:
+            df = SensorData.construct_empty_dataframe()
+        return df
+
+    @classmethod
+    def construct_empty_dataframe(cls) -> pd.DataFrame:
+        dict_base = {key: [] for key in cls._collect_fields().keys()}
+        df = pd.DataFrame(dict_base)
+        df = cls._convert_columns(df)
+        df = df.set_index(SensorReading._indexes, drop=True)
+        return df
