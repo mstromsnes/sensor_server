c77a98368c61018293bc8b6d8c94de87f1442777
Magne Strømsnes <alaka2@gmail.com>
2023-05-21 16:46:58 +0200

    Get rid of enum in favor of protocol implementation
    The enum was weird and implemented behaviour specific to a class it
    didn't relate to. It wasn't a member or in the inheritance hierarchy.
    
    This gets rid of that in favor of a Protocol inheritance hierarchy. An
    overarching SerializableFormat with static methods.
    This protocol is inherited by two more specialized protocols that know
    more specific types when dealing with Parquet and JSON. JSON serializes
    to strings, while Parquet serializes to bytes. A dataframe in general
    can be serialized to strings, bytes, or even buffers. This hierarchy
    allows us to express this in the type system.
    
    Actual implementation of the protocols should be done in classes next to
    schemas that then own the implementations. They know how to handle
    things like JSON orient= and the index in parquet for their schema.
    
    No other schemas are planned, but this allows us to extend with new ones
    if we need to.

--- format.py
+++ format.py
@@ -1,123 +1,86 @@
 import pandas as pd
+from pandas import DataFrame
 from io import BytesIO
-from typing import Union, Callable, Annotated, TypedDict, Any, get_type_hints, TypeVar
-from sensor import SensorData
-from enum import Enum, auto
+from typing import Union, Callable
 from pathlib import Path
+from typing import Protocol, runtime_checkable
 
+SerializedDataFrame = Union[str, bytes, BytesIO]
 
-def get_parquet(
-    df: pd.DataFrame,
-) -> bytes:
-    """Fastparquet version 2023.4.0 closes the file/buffer itself after its done. For a file this is ok. For a buffer this clears out the buffer, deleting the data.
-    When passing path=None to to_parquet() Pandas is supposed to return the bytes. But it does it the same way I tried. It uses a BytesIO buffer that gets closed,
-    then tries to return the bytes from the buffer, which aren't there. Code from pandas own documentation doesn't work because of this bug.
-
-    This is a workaround to prevent fastparquet from closing the buffer.
-    If using pyarrow we don't need to do this, but pyarrow doesn't install on the RPi easily.
-    """
-    buffer = BytesIO()
-    actual_close = buffer.close
-    buffer.close = lambda: None
-    df = df.reset_index()
-    df.to_parquet(buffer)
-    contents = buffer.getvalue()
-    actual_close()
-    return contents
-
-
-def write_parquet(df: pd.DataFrame, path: Path) -> None:
-    df = df.reset_index()
-    df.to_parquet(path)
-
-
-def get_json(df: pd.DataFrame) -> str:
-    return df.to_json(orient="table")
-
-
-def write_json(df: pd.DataFrame, path: Path) -> None:
-    df.to_json(path, orient="table")
-
-
-def read_json(json: str) -> pd.DataFrame:
-    return pd.read_json(json, orient="table")
-
-
-def read_parquet(archive: Union[BytesIO, bytes]) -> pd.DataFrame:
-    if isinstance(archive, bytes):
-        archive = BytesIO(archive)
-    df = pd.read_parquet(archive)
-    return SensorData.repair_dataframe(df)
-
-
-SerializedDataFrameType = TypeVar("SerializedDataFrameType", str, bytes, BytesIO)
-
-SerializeType = Callable[[pd.DataFrame], SerializedDataFrameType]
-DeserializeType = Callable[[SerializedDataFrameType], pd.DataFrame]
+SerializeType = Callable[[pd.DataFrame], SerializedDataFrame]
+DeserializeType = Callable[[SerializedDataFrame], DataFrame]
 WriteFnType = Callable[[pd.DataFrame, Path], None]
 
 
-class FormatAnnotation(TypedDict):
-    serialize: SerializeType
-    deserialize: DeserializeType
-    write: WriteFnType
-    endpoint: str
+def make_type_error(from_type: str, to_type: str) -> TypeError:
+    return TypeError(f"Cannot load {to_type} from {from_type}")
 
 
-class MissingAnnotationError(Exception):
-    def __init__(self, type):
-        self.message = f"{type=} does not have a type annotation. It should be annotated with functions to serialize, deserialize and write the archive to disk as well as an API endpoint."
-        super().__init__(self.message)
+class SerializationFormat(Protocol):
+    """Serialization format handler protocol that should be implemented separately for each schema the server handles."""
+
+    @staticmethod
+    def serialize(df: pd.DataFrame) -> SerializedDataFrame:
+        """Consumes an in-memory dataframe and outputs a serialized form"""
+        ...
+
+    @staticmethod
+    def write(df: pd.DataFrame, path: Path) -> None:
+        """Consumes an in-memory dataframe and writes it to file."""
+        ...
+
+    @staticmethod
+    def load(archive: Union[SerializedDataFrame, Path]) -> pd.DataFrame:
+        """Takes an in-memory serialized dataframe or path representing a file serialized dataframe and returns the dataframe."""
+        ...
+
+    @staticmethod
+    def endpoint() -> str:
+        """Returns the endpoint associated with this format/schema"""
+        ...
 
 
-ParquetIO = Annotated[
-    Any,
-    FormatAnnotation(
-        serialize=get_parquet,
-        deserialize=read_parquet,
-        write=write_parquet,
-        endpoint="parquet/",
-    ),
-]
-
-JSONIO = Annotated[
-    Any,
-    FormatAnnotation(
-        serialize=get_json, deserialize=read_json, write=write_json, endpoint="json/"
-    ),
-]
+# Below are more specialized formats that know more about what types they return and consume.
+# The formats are implemented near the Schema that holds
 
 
-class Format(Enum):
-    Parquet: ParquetIO = auto()
-    JSON: JSONIO = auto()
+@runtime_checkable
+class ParquetFormat(SerializationFormat, Protocol):
+    """Serialization handler for the Parquet format. Each schema that supports parquet should implement this protocol to allow serialization and loading."""
 
-    def _missing_annotation_error(self):
-        raise MissingAnnotationError(self)
+    @staticmethod
+    def serialize(df: pd.DataFrame) -> bytes:
+        ...
 
-    def _get_metadata(self, key: str) -> Any:
-        try:
-            annotations = get_type_hints(self, include_extras=True)[self.name]
-            metadata = annotations.__metadata__[0][key]
-        except (KeyError, ValueError, TypeError):
-            raise MissingAnnotationError(self)
-        return metadata
+    @staticmethod
+    def write(df: pd.DataFrame, path: Path) -> None:
+        ...
 
-    def serialize(self, df) -> SerializedDataFrameType:
-        """Serializes the dataframe using the serialize function attached to the format."""
-        output_fn = self._get_metadata("serialize")
-        return output_fn(df)
+    @staticmethod
+    def load(archive: Union[SerializedDataFrame, Path]) -> DataFrame:
+        ...
 
-    def load(self, archive) -> pd.DataFrame:
-        """Deserializes the dataframe using the deserialize function attached to the format."""
-        input_fn = self._get_metadata("deserialize")
-        return input_fn(archive)
+    @staticmethod
+    def endpoint() -> str:
+        ...
 
-    def write(self, archive, path):
-        write_fn = self._get_metadata("write")
-        write_fn(archive, path)
 
-    def endpoint(self) -> str:
-        """API endpoint for fastapi"""
-        endpoint = self._get_metadata("endpoint")
-        return endpoint
+@runtime_checkable
+class JSONFormat(SerializationFormat, Protocol):
+    """Serialization handler for the JSON format. Each schema that supports JSON should implement this protocol to allow serialization and loading."""
+
+    @staticmethod
+    def serialize(df: pd.DataFrame) -> str:
+        ...
+
+    @staticmethod
+    def write(df: pd.DataFrame, path: Path) -> None:
+        ...
+
+    @staticmethod
+    def load(archive: Union[str, Path]) -> DataFrame:
+        ...
+
+    @staticmethod
+    def endpoint() -> str:
+        ...
