f6908760747f2db4c884b9c2aa1b02ac458baae8
Magne Strømsnes <alaka@Alaka-PC>
2023-06-17 21:04:19 +0200

    Update parquet-manager to work with intervals

--- parquetmanager.py
+++ parquetmanager.py
@@ -1,7 +1,6 @@
-import datetime
-from abc import ABC, abstractclassmethod, abstractmethod
+from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Sequence, Iterable
 
 import pandas as pd
 
@@ -12,13 +11,13 @@ def start_of_week_timestamp(timestamp: pd.Timestamp):
     return start_of_week(*timestamp.isocalendar())
 
 
-def start_of_week(year: int, week: int, *args):
+def start_of_week(year: int, week: int, *args) -> pd.Timestamp:
     """Gets the date that starts given the iso week given"""
-    jan_fourth = datetime.datetime(
+    jan_fourth = pd.Timestamp(
         year, 1, 4
     )  # Jan 4th is always in the first ISO week of the year
-    start_of_week_one = jan_fourth - datetime.timedelta(days=jan_fourth.weekday())
-    start_of_week = start_of_week_one + datetime.timedelta(days=(week - 1) * 7)
+    start_of_week_one = jan_fourth - pd.Timedelta(days=jan_fourth.weekday())
+    start_of_week = start_of_week_one + pd.Timedelta(days=(week - 1) * 7)
     return start_of_week
 
 
@@ -27,32 +26,68 @@ def end_of_week(year: int, week: int, *args):
 
     This isn't simply start_of_week(..., week+1) as the next week can be the first in the next year.
     """
-    return start_of_week(year, week) + datetime.timedelta(days=7)
+    return start_of_week(year, week) + pd.Timedelta(days=7)
 
 
 class AbstractParquetManager(ABC):
+    @property
     @abstractmethod
-    def load_dataframe(self):
+    def empty(self) -> bool:
+        pass
+
+    @abstractmethod
+    def load_dataframe(self) -> pd.DataFrame:
         pass
 
     @abstractmethod
     def save_dataframe(self, dataframe: pd.DataFrame):
         pass
 
-    @abstractclassmethod
-    def earliest_date(cls) -> datetime.datetime:
-        pass
-
     @abstractmethod
     def get_historic_data(
-        self, start: datetime.datetime, end: Optional[datetime.datetime]
+        self, start: Optional[pd.Timestamp], end: Optional[pd.Timestamp]
     ) -> pd.DataFrame:
         pass
 
+    @abstractmethod
+    def oldest_date(self) -> pd.Timestamp:
+        pass
 
-class ParquetBackend:
+    @abstractmethod
+    def latest_date(self) -> pd.Timestamp:
+        pass
+
+
+class ArchiveBackend(ABC):
+    @abstractmethod
+    def __init__(self, _):
+        ...
+
+    @property
+    @abstractmethod
+    def empty(self) -> bool:
+        ...
+
+    @abstractmethod
+    def load_dataframe(self, key) -> pd.DataFrame:
+        ...
+
+    @abstractmethod
+    def save_dataframe(self, dataframe: pd.DataFrame, key) -> None:
+        ...
+
+    @abstractmethod
+    def archive_names(self) -> Sequence[str]:
+        ...
+
+
+class ParquetBackend(ArchiveBackend):
     def __init__(self, archive_folder: Path = Path("archive/")):
         self.archive_folder = archive_folder
+
+    @property
+    def empty(self) -> bool:
+        return not bool(list(self.archive_folder.glob("*")))
 
     def load_dataframe(self, parquet_file: Path) -> pd.DataFrame:
         try:
@@ -65,10 +100,17 @@ class ParquetBackend:
     def save_dataframe(self, dataframe: pd.DataFrame, parquet_file: Path):
         SensorData.Parquet.write(dataframe, self.archive_folder / parquet_file)
 
+    def archive_names(self) -> Iterable[str]:
+        return map(lambda path: path.stem, self.archive_folder.glob("*"))
 
-class DictBackend:
+
+class DictBackend(ArchiveBackend):
     def __init__(self, _: Optional[Path] = None):
         self.archive = dict()
+
+    @property
+    def empty(self) -> bool:
+        return not bool(self.archive)
 
     def load_dataframe(self, parquet_file: Path) -> pd.DataFrame:
         try:
@@ -76,13 +118,20 @@ class DictBackend:
         except KeyError:
             return SensorData.construct_empty_dataframe()
 
-    def save_dataframe(self, dataframe: pd.DataFrame, parquet_file: Path):
-        self.archive[parquet_file] = dataframe
+    def save_dataframe(self, dataframe: pd.DataFrame, archive_name: str):
+        self.archive[archive_name] = dataframe
+
+    def archive_names(self):
+        return self.archive.keys()
 
 
 class ParquetManager(AbstractParquetManager):
-    def __init__(self, backend=ParquetBackend(Path("archive/"))):
+    def __init__(self, backend: ArchiveBackend = ParquetBackend(Path("archive/"))):
         self.backend = backend
+
+    @property
+    def empty(self):
+        return self.backend.empty
 
     def load_dataframe(self) -> pd.DataFrame:
         last_week, today = self._get_recent_weeks()
@@ -91,7 +140,7 @@ class ParquetManager(AbstractParquetManager):
         return pd.concat([last_week_df, current_week_df])
 
     def _load_single_week(self, year: int, week: int, *args) -> pd.DataFrame:
-        parquet_file = self._archive_path_for_week(year, week)
+        parquet_file = self._archive_name_for_week(year, week)
         return self.backend.load_dataframe(parquet_file)
 
     def save_dataframe(self, dataframe: pd.DataFrame):
@@ -105,13 +154,14 @@ class ParquetManager(AbstractParquetManager):
         isoformat = week_starting_point.isocalendar()
         old_data = self._load_single_week(*isoformat)
         df = SensorData.repair_dataframe(pd.concat([old_data, dataframe])).sort_index()
-        self.backend.save_dataframe(df, self._archive_path_for_week(*isoformat))
+        self.backend.save_dataframe(df, self._archive_name_for_week(*isoformat))
 
     @staticmethod
     def _get_dataframe_week_ranges(df: pd.DataFrame) -> list[pd.Timestamp]:
         df = df.reset_index()
         week_series = (
-            df.timestamp.apply(start_of_week_timestamp)
+            df["timestamp"]
+            .apply(start_of_week_timestamp)
             .drop_duplicates()
             .sort_values(ignore_index=True)
         )
@@ -119,20 +169,29 @@ class ParquetManager(AbstractParquetManager):
 
     @staticmethod
     def _get_recent_weeks():
-        last_week = (datetime.date.today() - datetime.timedelta(days=7)).isocalendar()
-        today = datetime.date.today().isocalendar()
+        last_week = (pd.Timestamp.today() - pd.Timedelta(days=7)).isocalendar()
+        today = pd.Timestamp.today().isocalendar()
         return last_week, today
 
-    def _archive_path_for_week(self, year: int, week: int, *args) -> Path:
-        return Path(f"{year}-W{week:02}.parquet")
+    def _archive_name_for_week(self, year: int, week: int, *args) -> str:
+        return f"{year}-W{week:02}"
 
     @classmethod
-    def earliest_date(cls) -> datetime.datetime:
-        last_week, _ = cls._get_recent_weeks()
-        return start_of_week(*last_week)
+    def _name_to_timestamp(cls, name: str) -> pd.Timestamp:
+        return start_of_week(*map(int, name.split("-W")))
+
+    def oldest_date(self) -> pd.Timestamp:
+        assert not self.empty
+        return min(map(self._name_to_timestamp, self.backend.archive_names()))
+
+    def latest_date(self) -> pd.Timestamp:
+        assert not self.empty
+        return max(
+            map(self._name_to_timestamp, self.backend.archive_names())
+        ) + pd.Timedelta(days=7)
 
     def get_historic_data(
-        self, start: Optional[datetime.datetime], end: Optional[datetime.datetime]
+        self, start: Optional[pd.Timestamp], end: Optional[pd.Timestamp]
     ) -> pd.DataFrame:
         """The default behaviour is to only load the last two weeks of data. If data earlier than that is requested
         this function should be called to load it. It takes a beginning and optional end.
@@ -144,40 +203,47 @@ class ParquetManager(AbstractParquetManager):
         If they are in different years it loads the files from the given start date to the final week of the year, concates them and then concates them
         with a recursive call with the start_date of Jan 4 the next year.
         """
+        if self.backend.empty:
+            return SensorData.construct_empty_dataframe()
         df = self._load_old_data(start, end)
         return SensorData.repair_dataframe(df).sort_index()
 
     def _load_old_data(
-        self, start: Optional[datetime.datetime], end: Optional[datetime.datetime]
+        self, start: Optional[pd.Timestamp], end: Optional[pd.Timestamp]
     ) -> pd.DataFrame:
-        start_iso = start.isocalendar()
+        if start is None:
+            start = self.oldest_date()
         if end is None:
-            end = datetime.datetime.today()
+            end = self.latest_date()
+        return self._load_recursively(start, end)
+
+    def _load_recursively(self, start: pd.Timestamp, end: pd.Timestamp):
         if start > end:
             return SensorData.construct_empty_dataframe()
+
+        start_iso = start.isocalendar()
         end_iso = end.isocalendar()
-        if start_iso.year == end_iso.year:
-            if start_iso.week == end_iso.week:
-                return self._load_single_week(*start_iso)
-            else:
-                return pd.concat(
-                    [
-                        self._load_single_week(start_iso.year, week)
-                        for week in range(start_iso.week, end_iso.week + 1)
-                    ]
-                )
-        else:
-            year = start_iso.year
+        if start_iso.year != end_iso.year:  # type: ignore
+            year = start_iso.year  # type: ignore
             final_week = (
-                datetime.datetime(year=year, month=12, day=28).isocalendar().week
+                pd.Timestamp(year=year, month=12, day=28).isocalendar().week  # type: ignore
             )  # Dec 28 is ALWAYS in the last ISO week of the year
             frame = pd.concat(
                 [
                     self._load_single_week(year, week)
-                    for week in range(start_iso.week, final_week + 1)
+                    for week in range(start_iso.week, final_week + 1)  # type: ignore
                 ]
             )
-            new_year = datetime.datetime(
+            new_year = pd.Timestamp(
                 year=year + 1, month=1, day=4
             )  # Jan 4 is ALWAYS in the first ISO week of the year
-            return pd.concat([self.get_historic_data(new_year, end), frame])
+            return pd.concat([self._load_recursively(new_year, end), frame])
+
+        if start_iso.week == end_iso.week:  # type: ignore
+            return self._load_single_week(*start_iso)
+        return pd.concat(
+            [
+                self._load_single_week(start_iso.year, week)  # type: ignore
+                for week in range(start_iso.week, end_iso.week + 1)  # type: ignore
+            ]
+        )
--- tests/test_parquetmanager.py
+++ tests/test_parquetmanager.py
@@ -1,8 +1,8 @@
-from datastore import ParquetManager
-import datetime
-from tests.strats import sensor_data, MIN_TIMESTAMP, MAX_TIMESTAMP
 import pandas as pd
-from hypothesis import given
+from hypothesis import given, settings
+
+from datastore import ParquetManager
+from tests.strats import MAX_TIMESTAMP, MIN_TIMESTAMP, sensor_data
 
 
 def test_load_frame(parquet_manager_with_recent_data: ParquetManager):
@@ -12,29 +12,27 @@ def test_load_frame(parquet_manager_with_recent_data: ParquetManager):
 
 def test_load_historic_frame(parquet_manager: ParquetManager):
     frame = parquet_manager.get_historic_data(
-        MIN_TIMESTAMP - pd.Timedelta(days=1), None
+        MIN_TIMESTAMP - pd.Timedelta(days=1), MAX_TIMESTAMP + pd.Timedelta(days=1)
     )
     assert frame.size
 
 
 def test_future_data(parquet_manager: ParquetManager):
     frame = parquet_manager.get_historic_data(
-        MAX_TIMESTAMP + pd.Timedelta(days=1), None
+        MAX_TIMESTAMP + pd.Timedelta(days=1), MAX_TIMESTAMP + pd.Timedelta(days=365 * 4)
     )
     assert not frame.size
 
 
 @given(sensor_data=sensor_data())
+@settings(max_examples=10)
 def test_new_data_doesnt_overwrite_old_data(
     parquet_manager: ParquetManager, sensor_data: pd.DataFrame
 ):
     pre_historic = MIN_TIMESTAMP - pd.Timedelta(days=1)
     old_data = parquet_manager.get_historic_data(pre_historic, None)
     new_data = sensor_data
-    new_data = new_data.drop(new_data.isin(old_data), errors="ignore")
+    new_data = new_data.drop(new_data.isin(old_data), errors="ignore")  # type: ignore
     parquet_manager.save_dataframe(new_data)
     full_dataset = parquet_manager.get_historic_data(pre_historic, None)
-    try:
-        assert old_data.isin(full_dataset).all().all()
-    except:
-        assert False
+    assert old_data.isin(full_dataset).all().all()
